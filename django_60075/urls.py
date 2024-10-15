dd198905bfed4c38d93dfc53f7f855f41c530e08
Ronaldo Subelza <ronicabj2016@gmail.com>
2024-10-15 19:30:33 -0300

    Portfolio II,sin modelos

--- django_60075/urls.py
+++ django_60075/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import mi_vista
-# from .views import mi_vista
+from django.urls import path , include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mi-vista/', mi_vista)
+    path('', include('inicio.urls'))
 ]
---
+++ inicio/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from datetime import datetime
+from django.shortcuts import render
+
+def mi_vista(request):
+    return HttpResponse('hola soy la vista')
+def inicio(request):
+    return HttpResponse('<h1> Soy la pantalla de inicio </h1>')
+def vista_datos1(request, nombre):
+    nombre_mayuscula = nombre.upper()
+    return HttpResponse(f'Holaa {nombre_mayuscula}!!')
+def primer_template(request):
+    return HttpResponse('asd')
+def primer_template(request):
+    
+    
+    archivo_del_template = open(r'templates\primer_template.html')
+    template = Template(archivo_del_template.read())
+    archivo_del_template.close()
+    contexto = Context()
+    
+    render_template = template.render(contexto)
+    
+    return HttpResponse(render_template)
+
+def segundo_template(request):
+    
+    fecha_actual = datetime.now()
+    datos = {
+        'fecha_actual': fecha_actual,
+        'numeros':list(range(1, 11))    
+    }
+    
+    #v1
+    # with open(r'templates\segundo_template.html') as archivo_del_template:
+    #     template = Template(archivo_del_template.read())
+    # contexto = Context(datos)
+    # render_template = template.render(contexto)
+    
+    #v2
+    # template = loader.get_template('segundo_template.html')
+    # render_template = template.render(datos)
+    # return HttpResponse(render_template)
+
+    #V3
+    
+    return render(request, 'segundo_template.html',datos)
+    
